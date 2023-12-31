550e610a477368df9b91944d53cfc8195e19d24f
Andrew Strelsky <ajs222@njit.edu>
2023-12-31 11:46:33 -0500

    removed usage of RawSyscall6

--- src/syscall/syscall_prospero_amd64.go
+++ src/syscall/syscall_prospero_amd64.go
@@ -134,7 +134,7 @@ func DynlibGetInfo(handle uint32) (*ModuleInfo, Errno) {
 }
 
 func _setsockopt(s int, level int, name int, val unsafe.Pointer, vallen uintptr) (err error) {
-	_, _, e1 := RawSyscall6(SYS_SETSOCKOPT, uintptr(s), uintptr(level), uintptr(name), uintptr(val), uintptr(vallen), 0)
+	_, _, e1 := Syscall6(SYS_SETSOCKOPT, uintptr(s), uintptr(level), uintptr(name), uintptr(val), uintptr(vallen), 0)
 	if e1 != 0 {
 		err = errnoErr(e1)
 	}
